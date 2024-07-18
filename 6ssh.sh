a610c41e9afa57f18d994f5017f520c99efeccfa
Craig Miller <cvmiller@gmail.com>
2024-07-18 23:03:41 +0000

    Inital commit

---
+++ 6ssh.sh
@@ -0,0 +1,140 @@
+#!/usr/bin/env bash
+
+#
+#	6ssh: a shell program to ssh _from_ stable SLAAC Address
+#
+#	by Craig Miller
+#
+#	16 July 2024
+
+
+# todo:
+#	1) allow arbitrary ssh options (e.g. -o AddressFamily=inet6
+#	2) support BSD
+
+
+function usage {
+               echo "	$0 - ssh using Stable SLAAC Source Address "
+	       echo "	e.g. $0 <host> "
+	       echo "	-i  use this interface"
+	       echo "	-X  use X forwarding"
+	       echo "	"
+	       echo " By Craig Miller - Version: $VERSION"
+	       exit 1
+           }
+
+VERSION=0.9.1
+
+# some variables
+
+DEBUG=0
+
+# ULA prefixes start with fd
+PREFIX='fd'
+# GUA prefixes start with 2
+#
+# Comment out next line, if using ULAs
+PREFIX='2'
+
+IPV6_REG='[0-9a-f]{2,3}:([0-9a-f]+:){6}[0-9a-f]+'
+IPV6_REG='[0-9a-f]{2,3}:([0-9a-f]+:){3,6}[:]?[0-9a-f]+'
+INTERFACE=""
+
+SSHOPTS=""
+
+numopts=0
+while getopts "?hdi:XY" options; do
+  case $options in
+    i ) INTERFACE=$OPTARG
+    	numopts=$(( numopts + 2));;
+    d ) DEBUG=1
+    	(( numopts++));;
+    X ) SSHOPTS="$SSHOPTS -X"
+    	(( numopts++));;
+    Y ) SSHOPTS="$SSHOPTS -Y"
+    	(( numopts++));;
+    h ) usage;;
+    \? ) usage;;		# show usage with flag and no value
+    * ) usage;;		# show usage with unknown flag
+  esac
+done
+# remove the options as cli arguments
+shift $numopts
+
+#debug
+#echo "$#	$1"
+
+# get HOSTNAME of target
+HOST=""
+if [ -n "$1" ]; then
+	HOST="$1"
+else
+	echo "Error: no host specified"
+	usage
+fi
+
+
+function get_slaac_addr  {
+	local local_intf="$1"
+	# get IPv6 Stable SLAAC Address
+	slaac_addr=""
+	slaac_addr=$(ip addr show $local_intf | grep -E '(mngtmpaddr|noprefixroute)' |  grep -o -E "$PREFIX$IPV6_REG" | tail -1)	
+	#if (( DEBUG == 1 )); then echo "DEBUG: slaac_addr: $slaac_addr";fi
+	echo -e "$slaac_addr"
+}
+ 
+
+slaac_addr=""
+if [ -z "$INTERFACE" ]; then
+	# set up a list of active interfaces
+	INTF_LIST=$(ip link | grep LOWER_UP | grep -v LOOPBACK | cut -d ':' -f 2 | tr '\n' ' ' )
+	if (( DEBUG == 1 )); then echo "DEBUG:  INTF_LIST:$INTF_LIST";fi
+
+	list_length=$(wc -w <<< "$INTF_LIST")
+	if [ $list_length -gt 1 ]; then
+		for intf in $INTF_LIST
+		do
+			tmp_addr=$(get_slaac_addr "$intf")
+			if (( DEBUG == 1 )); then echo "DEBUG:  intf:$intf 	tmp_addr:$tmp_addr";fi
+			if [ -n "$tmp_addr" ]; then
+				slaac_addr="$tmp_addr"
+				INTF="$intf"
+				break
+			fi
+		done
+		#error if no address found
+		if [ -z "$slaac_addr" ]; then
+			echo "Error: no IPv6 address found on interfaces"
+			exit 1
+		fi
+	else
+		slaac_addr=$(get_slaac_addr "$INTF_LIST")
+		INTF="$INTF_LIST"
+	fi
+else
+	# User specified interface
+	INTF=$(ip link | grep LOWER_UP | cut -d ':' -f 2 | grep -o " $INTERFACE" )
+	if [ -z "$INTF" ]; then
+		echo "Error: Selected Interface: $INTERFACE is not found or DOWN"
+		usage
+	fi
+	slaac_addr=$(get_slaac_addr "$INTF")
+	if [ -z "$slaac_addr" ]; then
+		echo "Error: No IPv6 address found on $INTERFACE"
+		exit 1
+	fi
+fi
+
+
+if (( DEBUG == 1 )); then echo "DEBUG:  INTF:$INTF	SLAAC ADDR=$slaac_addr";fi
+
+
+
+
+# OK, lets ssh to host using Stable SLAAC Addr as source
+ssh -b $slaac_addr $SSHOPTS "$HOST"
+
+echo "6ssh: Pau"
+
+
+
---
+++ README.md
@@ -0,0 +1,57 @@
+## Synopsis
+
+A bash script which calls `ssh` bound to a the local IPv6 Stable SLAAC address. Enables persistent `ssh` sessions across laptop sleep/awake cycles.
+
+### Why?
+
+By default IPv6 will use temporary addresses ([RFC 8981](https://www.rfc-editor.org/rfc/rfc8981)) for outbound connections, including `ssh`. While it possible to disable this *feature*, in most cases I prefer temporary addresses to be used (e.g. `https` sessions). 
+
+However, in the modern world, putting a laptop to sleep, will cause `systemd` to generate new temporary addresses, and the `ssh` connection will **not** be restored after waking up the laptop.
+
+By *binding* the `ssh` session to a local Stable SLAAC address, the `ssh` session will remain connected after the laptop wakes up from sleep and reconnects to the network.
+
+## Motivation
+
+Apparently, I put my laptop to sleep a lot. I grew tired of re-establishing my `ssh` session, and all my GUI X-forwarded apps (mostly editors) as well.
+
+By *binding* the `ssh` session with the `-b <address>` parameter, the sessions remain up, even through short sleeps.
+
+This script merely automates the process of finding which interface has a valid Stable SLAAC Address, and then calls `ssh` with the appropriate *bind address*. 
+
+
+## The Script
+
+The script checks all the interfaces for a Stable SLAAC Address, and uses the first one it finds for the `ssh` session. 
+
+### Help
+
+Like any script there is help. With this first release there is only `-i <interface>` which allows the user to control which interface is used, and `-X` for X11 forwarding. 
+
+```
+$ ./6ssh.sh -h
+	./6ssh.sh - ssh using Stable SLAAC Source Address 
+	e.g. ./6ssh.sh <host> 
+	-i  use this interface
+	-X  use X forwarding
+	
+```
+
+
+### Why Bash?
+
+Bash is terrible at string handling, why write this script in bash? Because I wanted it to run on my router (OpenWRT), and just about every where else, with the minimal amount of dependencies. It is possible to run Python on OpenWRT, but Python requires more storage (more packages) than just `bash`.
+
+
+
+## Dependencies
+The script is dependent on `bash`, `ip`, and `grep`, both of which should be readily available on any linux distro.
+
+
+## Contributors
+
+All code by Craig Miller cvmiller at gmail dot com. But ideas, and ports to other languages are welcome. 
+
+
+## License
+
+This project is open source, under the GPLv2 license (see [LICENSE](LICENSE))
